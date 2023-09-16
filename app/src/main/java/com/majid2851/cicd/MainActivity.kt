381a878f84277392bd505c98e599b83dd2012458
majid2851 <majidbagheri2851@gmail.com>
2023-09-16 20:09:52 +0430

    test Firebase 5

--- app/src/main/java/com/majid2851/cicd/MainActivity.kt
+++ app/src/main/java/com/majid2851/cicd/MainActivity.kt
@@ -3,6 +3,7 @@ package com.majid2851.cicd
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
