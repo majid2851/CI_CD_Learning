24b4928ad5c993c1b37681fc363e4ae9db0a95c3
majid2851 <majidbagheri2851@gmail.com>
2023-09-16 14:49:15 +0430

    test building 24

--- app/src/main/java/com/majid2851/cicd/MainActivity.kt
+++ app/src/main/java/com/majid2851/cicd/MainActivity.kt
@@ -8,6 +8,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+          mag()
+    }
+    fun mag()
+    {
+                            System.out.println()
+
     }
 }
 
